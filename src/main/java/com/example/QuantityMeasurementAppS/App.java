8c4d0af1530fbef9f474316d1aa1a630337fe57b
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-03-09 11:18:46 +0530

    UC 7

--- src/main/java/com/example/QuantityMeasurementAppS/App.java
+++ src/main/java/com/example/QuantityMeasurementAppS/App.java
@@ -4,29 +4,36 @@ public class App {
 
     public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
+        QuantityLength a = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength b = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        QuantityLength result = q1.add(q2);
-
-        System.out.println("Result: " + result);
-
-
-        QuantityLength a = new QuantityLength(12.0, LengthUnit.INCHES);
-        QuantityLength b = new QuantityLength(1.0, LengthUnit.FEET);
-
-        System.out.println("Result: " + a.add(b));
-
+        System.out.println(a.add(b, LengthUnit.FEET));
+        System.out.println(a.add(b, LengthUnit.INCHES));
+        System.out.println(a.add(b, LengthUnit.YARDS));
 
         QuantityLength c = new QuantityLength(1.0, LengthUnit.YARDS);
         QuantityLength d = new QuantityLength(3.0, LengthUnit.FEET);
 
-        System.out.println("Result: " + c.add(d));
+        System.out.println(c.add(d, LengthUnit.YARDS));
 
+        QuantityLength e = new QuantityLength(36.0, LengthUnit.INCHES);
+        QuantityLength f = new QuantityLength(1.0, LengthUnit.YARDS);
 
-        QuantityLength e = new QuantityLength(2.54, LengthUnit.CENTIMETERS);
-        QuantityLength f = new QuantityLength(1.0, LengthUnit.INCHES);
+        System.out.println(e.add(f, LengthUnit.FEET));
 
-        System.out.println("Result: " + e.add(f));
+        QuantityLength g = new QuantityLength(2.54, LengthUnit.CENTIMETERS);
+        QuantityLength h = new QuantityLength(1.0, LengthUnit.INCHES);
+
+        System.out.println(g.add(h, LengthUnit.CENTIMETERS));
+
+        QuantityLength i = new QuantityLength(5.0, LengthUnit.FEET);
+        QuantityLength j = new QuantityLength(0.0, LengthUnit.INCHES);
+
+        System.out.println(i.add(j, LengthUnit.YARDS));
+
+        QuantityLength k = new QuantityLength(5.0, LengthUnit.FEET);
+        QuantityLength l = new QuantityLength(-2.0, LengthUnit.FEET);
+
+        System.out.println(k.add(l, LengthUnit.INCHES));
     }
 }
